5e649b30f78cd7cb94454c2143e141993bacc32d
hakhanhne <s3877707@rmit.edu.vn>
2023-01-12 11:42:45 +0700

    initial commit

---
+++ js/components/header.js
@@ -0,0 +1,13 @@
+document.querySelector("header").innerHTML =
+`
+<div class="flex-row space-between">
+  <div class="header-title">Phuong Hai scientific</div>
+  <div class="flex-row align-center">
+    <a href="index.html">Home</a>
+    |
+    <a href="admin_manage.html">Admin</a>
+    <button class="btn btn-white-text btn-transparent btn-sm" id="btn-signout">Sign Out</button> 
+  </div>
+</div>
+
+`;
